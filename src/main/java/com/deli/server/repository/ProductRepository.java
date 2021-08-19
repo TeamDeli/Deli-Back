efd66b1ce123540724bf451b60ae911dc9c09db8
lizuAg <wndms058@gmail.com>
2021-08-20 02:49:27 +0900

    refactor: 불필요한 import 삭제

--- src/main/java/com/deli/server/repository/ProductRepository.java
+++ src/main/java/com/deli/server/repository/ProductRepository.java
@@ -1,7 +1,7 @@
 package com.deli.server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+//import org.springframework.stereotype.Repository;
 
 import com.deli.server.model.Product;
 

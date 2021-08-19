44d352a06b708d72c05e7cf1449ecc80dea47bd1
Kiltae <loverpleasestayo0@gmail.com>
2021-08-19 21:41:45 +0900

    feat: Repository 작성

--- src/main/java/com/deli/server/repository/AnniversaryRepository.java
+++ src/main/java/com/deli/server/repository/AnniversaryRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 import com.deli.server.model.Anniversary;
 
 public interface AnniversaryRepository extends JpaRepository<Anniversary,Integer>{
-
 }

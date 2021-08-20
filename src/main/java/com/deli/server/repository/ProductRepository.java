cef85c8bc30b2bbc184552a9ea36c48b1aee5429
kyungminlee-12 <min2000.lee@gmail.com>
2021-08-20 14:23:45 +0900

    feat: Product Repository 코드

--- src/main/java/com/deli/server/repository/ProductRepository.java
+++ src/main/java/com/deli/server/repository/ProductRepository.java
@@ -1,10 +1,12 @@
 package com.deli.server.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.deli.server.model.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 }

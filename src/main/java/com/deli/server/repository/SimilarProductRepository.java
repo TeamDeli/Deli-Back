e1bdd6bc8c1e979732beb0ed9a83990b1219fe5e
kyungminlee-12 <min2000.lee@gmail.com>
2021-08-20 14:33:06 +0900

    feat: Similar Product Repository

--- src/main/java/com/deli/server/repository/SimilarProductRepository.java
+++ src/main/java/com/deli/server/repository/SimilarProductRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.deli.server.model.SimilarProduct;
 
 public interface SimilarProductRepository extends JpaRepository<SimilarProduct, Integer>{
-	List<SimilarProduct> findByUserId(int id);
 
 }

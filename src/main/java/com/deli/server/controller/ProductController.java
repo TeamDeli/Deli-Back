42ea610e69051b99dca323866b73b5ad09b8bf56
bsorinnn <bxxsxrxx@gmail.com>
2021-08-20 15:30:24 +0900

    feat: productController 삭제 후 ProductController로 바꿈

--- src/main/java/com/deli/server/controller/ProductController.java
+++ src/main/java/com/deli/server/controller/ProductController.java
@@ -17,41 +17,41 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/product")
 public class ProductController {
-    private final ProductRepository productRepository;
-    private final ProductService productService;
+	private final ProductRepository productRepository;
+	private final ProductService productService;
 
-    @CrossOrigin(origins = "*")
-    @GetMapping("/getProducts")
-    public ResponseEntity<?> findAll(){
-        return new ResponseEntity<>(productService.findAll(), HttpStatus.OK); // 200
-    }
+	@CrossOrigin(origins = "*")
+	@GetMapping("/getProducts")
+	public ResponseEntity<?> findAll(){
+		return new ResponseEntity<>(productService.findAll(), HttpStatus.OK); // 200
+	}
 
-    @CrossOrigin(origins = "*")
-    @PostMapping("/getProducts")
-    public ResponseEntity<?> searchMember(@RequestBody ProductSearchCondition condition){
-        System.out.println(condition.getGender());
-        System.out.println(condition.getAge());
-        System.out.println(condition.getPrice());
-        System.out.println(condition.getPrice().get(0));
-        System.out.println(condition.getPrice().get(1));
-        List<ProductListDto> result = productRepository.search(condition);
-        System.out.println(result);
-        return new ResponseEntity<List<ProductListDto>>(result, HttpStatus.OK);
-    }
+	@CrossOrigin(origins = "*")
+	@PostMapping("/getProducts")
+	public ResponseEntity<?> searchMember(@RequestBody ProductSearchCondition condition){
+		System.out.println(condition.getGender());
+		System.out.println(condition.getAge());
+		System.out.println(condition.getPrice());
+		System.out.println(condition.getPrice().get(0));
+		System.out.println(condition.getPrice().get(1));
+		List<ProductListDto> result = productRepository.search(condition);
+		System.out.println(result);
+		return new ResponseEntity<List<ProductListDto>>(result, HttpStatus.OK);
+	}
 
-    @CrossOrigin
-    @GetMapping("/{id}")
-    public ResponseEntity<?> productById(@PathVariable int id){
-        Product p = productService.getProductById(id);
-        System.out.println(p);
-        return new ResponseEntity<>(productService.getProductById(id),HttpStatus.OK);
-    }
+	@CrossOrigin
+	@GetMapping("/{id}")
+	public ResponseEntity<?> productById(@PathVariable int id){
+		Product p = productService.getProductById(id);
+		System.out.println(p);
+		return new ResponseEntity<>(productService.getProductById(id),HttpStatus.OK);
+	}
 
-    @CrossOrigin
-    @GetMapping("/search")
-    public ResponseEntity<?> searchKeywords(@RequestParam(value="keyword") String keyword) {
-        List<ProductListDto> searchResult = productService.searchPosts(keyword);
-        return new ResponseEntity<List<ProductListDto>>(searchResult, HttpStatus.OK);
-    }
+	@CrossOrigin
+	@GetMapping("/search")
+	public ResponseEntity<?> searchKeywords(@RequestParam(value="keyword") String keyword) {
+		List<ProductListDto> searchResult = productService.searchPosts(keyword);
+		return new ResponseEntity<List<ProductListDto>>(searchResult, HttpStatus.OK);
+	}
 
 }
